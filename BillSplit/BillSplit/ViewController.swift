9f1058ab5cc4671f75660c31c5d156ec5a4914ed
Le0nX <cloudmindone@gmail.com>
2019-09-14 16:08:45 +0300

    init

---
+++ BillSplit/BillSplit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BillSplit
+//
+//  Created by Denis Nefedov on 14.09.2019.
+//  Copyright © 2019 X. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
